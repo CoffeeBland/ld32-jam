a3f07dcc6b1f498c76164155804cd388e00989d3
Frederic Gingras <frederic@gingras.cc>
2015-04-18 19:34:50 -0400

    Adapt to new font api

--- core/src/com/dagothig/knightfight/state/SplashState.java
+++ core/src/com/dagothig/knightfight/state/SplashState.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.dagothig.knightfight.util.ColorUtil;
 import com.dagothig.knightfight.util.FontUtil;
@@ -11,19 +12,23 @@ import com.dagothig.knightfight.util.FontUtil;
 public class SplashState extends AutoSwitchState {
     @Override protected float getDuration() { return 2000; }
     @Override protected Class<? extends State> getNextStateClass() { return PlayerState.class; }
-    @Override protected Color getTransitionColor() { return new Color(0xefe8e100); }
+    @Override protected Color getTransitionColor() { return new Color(0xFFFFFFFF); }
     @Override protected float getTransitionDuration() { return TRANSITION_MEDIUM; }
 
     private static String LOGO_PATH = "coffeebland.png";
     private Texture bg;
     private Texture logo;
     private BitmapFont font;
+    private GlyphLayout text;
 
     public SplashState() {
         setBackgroundColor(Color.WHITE.cpy());
         bg = ColorUtil.whitePixel();
-        font = FontUtil.normalFont();
         logo = new Texture(Gdx.files.internal(LOGO_PATH));
+
+        font = FontUtil.normalFont();
+        font.setColor(Color.BLACK.cpy());
+        text = new GlyphLayout(font, "Catiniata - Dagothig - Kiasaki");
     }
 
     @Override
@@ -31,12 +36,10 @@ public class SplashState extends AutoSwitchState {
         float imgX = (Gdx.graphics.getWidth() / 2) - (logo.getWidth() / 2); // Half the logo
         float imgY = (Gdx.graphics.getHeight() / 2) - (logo.getHeight() / 2); // Half the logo
 
-        batch.setColor(new Color(0x75858AFF));
+        batch.setColor(new Color(0xFEFEFEFF));
         batch.draw(bg, 12, 12, Gdx.graphics.getWidth()-24, Gdx.graphics.getHeight()-24);
         batch.setColor(Color.WHITE.cpy());
         batch.draw(logo, imgX, imgY);
-        font.setColor(Color.BLACK.cpy());
-        String text = "Catiniata - Dagothig - Kiasaki";
-        font.draw(batch, text, (Gdx.graphics.getWidth()/2)-(font.getSpaceWidth()/2), 48);
+        font.draw(batch, text, (Gdx.graphics.getWidth()/2)-(text.width/2), 48);
     }
 }
